617afc0cfd4defc5cb47b20343bd10a6079c37ef
Prabesh Maharjan <prabeshmrz.pm@gmail.com>
2021-06-17 17:58:50 +0545

    THEME AND TERMINAL
    - floatterm attached to bottom of screen
    - material dark theme set

---
+++ lua/lc-floaterm/init.lua
@@ -0,0 +1,2 @@
+vim.g.floaterm_wintype="split"
+vim.g.floaterm_height=15
--- lua/theme/material.lua
+++ lua/theme/material.lua
@@ -1,10 +1,10 @@
 ---@diagnostic disable: undefined-global
 -- Example config in lua
-vim.g.material_style = 'ocean darker'
-vim.g.material_italic_comments = true
-vim.g.material_italic_keywords = true
-vim.g.material_italic_functions = true
-vim.g.material_italic_variables = true
+vim.g.material_style = 'darker'
+vim.g.material_italic_comments = false
+vim.g.material_italic_keywords = false
+vim.g.material_italic_functions = false
+vim.g.material_italic_variables = false
 vim.g.material_contrast = true
 vim.g.material_borders = false
 vim.g.material_disable_background = false
